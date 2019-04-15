6c5da0471c0d0c5af61e0fc2f0fde66fe6149d5e
your <jurgenkela@gmail.com>
2019-04-15 19:05:28 -0400

    maxProfits solved

--- Leetcode/maxProfit.js
+++ Leetcode/maxProfit.js
@@ -12,7 +12,13 @@
 //              Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
 
 var maxProfit = function(prices) {
-  return prices;
+  let maxProfit = 0;
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i + 1] > prices[i]) {
+      maxProfit += prices[i + 1] - prices[i];
+    }
+  }
+  return maxProfit;
 };
 
-console.log([7, 1, 5, 3, 6, 4]);
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
