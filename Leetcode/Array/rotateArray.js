095d55b200e771ea3b680c86e2108164cd00bb84
your <jurgenkela@gmail.com>
2019-04-15 20:28:01 -0400

    rotateArray solved

--- Leetcode/Array/rotateArray.js
+++ Leetcode/Array/rotateArray.js
@@ -8,3 +8,24 @@
 // rotate 1 steps to the right: [7,1,2,3,4,5,6]
 // rotate 2 steps to the right: [6,7,1,2,3,4,5]
 // rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+var rotate = function(nums, k) {
+  if (nums === null || nums.length < 2) return;
+  k %= nums.length;
+  reverse(nums, 0, nums.length - 1);
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, nums.length - 1);
+  return nums;
+};
+
+var reverse = function(nums, start, end) {
+  while (start < end) {
+    temp = nums[start];
+    nums[start] = nums[end];
+    nums[end] = temp;
+    start++;
+    end--;
+  }
+};
+
+console.log(rotate([1, 2, 3, 4, 5, 6, 7], 1));
