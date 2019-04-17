583dda24f54028e1aeaf4793af5d7d14dcaeef4b
your <jurgenkela@gmail.com>
2019-04-17 18:40:54 -0400

    moveZeros solution

--- Leetcode/Array/moveZeroes.js
+++ Leetcode/Array/moveZeroes.js
@@ -6,6 +6,15 @@
 // Output: [1,3,12,0,0]
 
 var moveZeroes = function(nums) {
+  let index = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] != 0) {
+      nums[index++] = nums[i];
+    }
+  }
+  while (index < nums.length) {
+    nums[index++] = 0;
+  }
   return nums;
 };
 
