aadcdef8bfbe2431ea5b4252a0f33b732a09fe66
your <jurgenkela@gmail.com>
2019-04-17 10:43:22 -0400

    intersect init

---
+++ Leetcode/Array/intersect.js
@@ -0,0 +1,17 @@
+// Given two arrays, write a function to compute their intersection.
+
+// Example 1:
+
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2,2]
+
+// Note:
+
+// Each element in the result should appear as many times as it shows in both arrays.
+// The result can be in any order.
+
+var intersect = function(nums1, nums2) {
+  return nums1 + nums2;
+};
+
+console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4]));
