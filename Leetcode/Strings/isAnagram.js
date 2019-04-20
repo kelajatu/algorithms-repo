a81f6ebb531c7b802fd8199e64464cbe46ab65eb
your <jurgenkela@gmail.com>
2019-04-19 21:05:20 -0400

    init latest problems

---
+++ Leetcode/Strings/isAnagram.js
@@ -0,0 +1,10 @@
+// Given two strings s and t , write a function to determine if t is an anagram of s.
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+
+const isAnagram = (s, t) => {
+  return s;
+};
