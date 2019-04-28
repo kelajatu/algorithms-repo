fd738cf2908c76ffc3e0824221ebe67302895031
your <jurgenkela@gmail.com>
2019-04-28 12:01:11 -0400

    reverseInt solution

--- Leetcode/Strings/reverseInteger.js
+++ Leetcode/Strings/reverseInteger.js
@@ -10,5 +10,15 @@
 // Output: -321
 
 const reverse = x => {
-  return x;
+  const reversed =
+    parseInt(
+      Math.abs(x)
+        .toString()
+        .split("")
+        .reverse()
+        .join("")
+    ) * Math.sign(x);
+  return reversed <= 0x7fffffff && reversed >= -0x80000000 ? reversed : 0;
 };
+
+console.log(reverse(-120));
