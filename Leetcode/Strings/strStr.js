bcc590b7e9954bf01386b6671e7ebbeb27900ee6
your <jurgenkela@gmail.com>
2019-04-29 10:13:25 -0400

    strStr init

---
+++ Leetcode/Strings/strStr.js
@@ -0,0 +1,14 @@
+// Implement strStr().
+
+// Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+// Example 1:
+
+// Input: haystack = "hello", needle = "ll"
+// Output: 2
+
+const strStr = (haystack, needle) => {
+  return needle;
+};
+
+console.log(strStr("aaaaa", "bba"));
