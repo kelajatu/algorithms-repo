3b41287aa05b7ae7c41387f795098225ece78822
your <jurgenkela@gmail.com>
2019-05-03 20:10:28 -0400

    day1 init, day2 solution

--- HackerRank/30 Days of Code/Day2.js
+++ HackerRank/30 Days of Code/Day2.js
@@ -27,7 +27,10 @@
 // 15
 
 function solve(meal_cost, tip_percent, tax_percent) {
-  return meal_cost;
+  let tip = meal_cost * (tip_percent / 100);
+  let tax = meal_cost * (tax_percent / 100);
+  let total = Math.round(meal_cost + tip + tax);
+  console.log(total);
 }
 
 console.log(solve(12.0, 20, 8));
