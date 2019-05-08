7f34376323ef6df4a8fdd9d086eb576f7b9dbacf
your <jurgenkela@gmail.com>
2019-05-08 09:36:52 -0400

    removeDuplicates solution

--- Thatjsdude/Part 1/removeDuplicate.js
+++ Thatjsdude/Part 1/removeDuplicate.js
@@ -2,6 +2,17 @@
 
 // Answer: will start a while looping and keep an object/ associated array. If i find an element for the first time i will set its value as true (that will tell me element added once.). if i find a element in the exists object, i will not insert it into the return array.
 
-function removeDuplicate(arr) {}
+function removeDuplicate(arr) {
+  var exists = {},
+    outArr = [];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (!exists[arr[i]]) {
+      outArr.push(arr[i]);
+      exists[arr[i]] = true;
+    }
+  }
+  return outArr;
+}
 
 console.log(removeDuplicate([1, 3, 3, 3, 1, 5, 6, 7, 8, 1]));
