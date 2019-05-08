5e42a199ae5505df351f79428e28f42c75af8df1
your <jurgenkela@gmail.com>
2019-05-08 09:43:07 -0400

    mergeSortedArray solution

---
+++ Thatjsdude/Part 1/mergeSortedArray.js
@@ -0,0 +1,27 @@
+// Question: How would you merge two sorted array?
+
+// Answer: I will keep a pointer for each array and (read the code. and be careful about this one.)
+
+function mergeSortedArray(a, b) {
+  var merged = [],
+    aElm = a[0],
+    bElm = b[0],
+    i = 1,
+    j = 1;
+
+  if (a.length === 0) return b;
+  if (b.length === 0) return a;
+
+  while (aElm || bElm) {
+    if ((aElm && !bElm) || aElm < bElm) {
+      merged.push(aElm);
+      aElm = a[i++];
+    } else {
+      merged.push(bElm);
+      bElm = b[j++];
+    }
+  }
+  return merged;
+}
+
+console.log(mergeSortedArray([2, 5, 6, 9], [1, 2, 3, 29]));
