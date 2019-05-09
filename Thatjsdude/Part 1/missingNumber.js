d2535d873ea1b19cd904595a4eb9565dc97297d4
your <jurgenkela@gmail.com>
2019-05-09 12:33:42 -0400

    missingNumber solution

--- Thatjsdude/Part 1/missingNumber.js
+++ Thatjsdude/Part 1/missingNumber.js
@@ -8,7 +8,7 @@ function missingNumber(arr) {
   var n = arr.length + 1,
     sum = 0,
     expectedSum = (n * (n + 1)) / 2;
-  for (var i = 0, len = arr.length; i < len; i++) {
+  for (var i = 0; i < arr.length; i++) {
     sum += arr[i];
   }
   return expectedSum - sum;
