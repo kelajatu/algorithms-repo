76204ce9f999e1037c62ed5147789dcaa1799484
your <jurgenkela@gmail.com>
2019-05-14 12:58:41 -0400

    romanToInt solution

--- Leetcode/Math/romanToInt.js
+++ Leetcode/Math/romanToInt.js
@@ -23,7 +23,30 @@
 // Output: 3
 
 var romanToInt = function(s) {
-  return s;
+  var result = 0;
+  var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var roman = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I"
+  ];
+  for (var i = 0; i <= decimal.length; i++) {
+    while (s.indexOf(roman[i]) === 0) {
+      result += decimal[i];
+      s = s.replace(roman[i], "");
+    }
+  }
+  return result;
 };
 
 console.log(romanToInt("IV"));
