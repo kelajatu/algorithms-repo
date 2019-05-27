faf86aac30ab24023f791e36640cdb8db0c94579
your <jurgenkela@gmail.com>
2019-05-27 11:40:46 -0400

    grokking algos init

---
+++ CCI/arraysAndStrings.java
@@ -0,0 +1,13 @@
+boolean isUniqueChars(String str){
+    if(str.length() > 128) return false;
+
+    boolean[] char_set = new boolean[128]
+    if(int i = 0; i < str.length(); i++){
+        int val = str.charAt(i);
+        if (char_set[val]) {
+            return false;
+        }
+        char_set[val] = true;
+    }
+    return true;
+}
