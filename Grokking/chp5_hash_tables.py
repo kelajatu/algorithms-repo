d9127f8423606860f23a66792a3beb5edbad0579
your <jurgenkela@gmail.com>
2019-05-30 09:36:39 -0400

    hash tables example

---
+++ Grokking/chp5_hash_tables.py
@@ -0,0 +1,11 @@
+# FB Cache example?
+
+# cache = {}
+
+# def det_page(url):
+#     if cache.get(url):
+#         return cache[url]
+#     else:
+#         data = get_data_from_server(url)
+#         cache[url] = data
+#         return data
