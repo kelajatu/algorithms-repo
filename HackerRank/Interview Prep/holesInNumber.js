652c10011be3badc4579ee95922c77dd18f20bf8
your <jurgenkela@gmail.com>
2019-06-04 16:44:19 -0400

    update

--- HackerRank/Interview Prep/holesInNumber.js
+++ HackerRank/Interview Prep/holesInNumber.js
@@ -1,5 +1,4 @@
 function countHoles(num) {
-  // Write your code here
   var digits = num.toString().split("");
   var values = [1, 0, 0, 0, 1, 0, 1, 0, 2, 1];
   return digits.reduce((total, index) => {
